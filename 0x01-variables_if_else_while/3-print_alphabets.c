2192b32eeda345605bc28d44c6fc86f385c1a4a2
Wende924 <wende924@gmail.com>
2023-03-18 02:12:14 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char c;
+		/* Print lowercase alphabet*/
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	/* Print uppercase alphabet*/
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	/* Print new line*/
+	putchar('\n');
+	return (0);
+}
